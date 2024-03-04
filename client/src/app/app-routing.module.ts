d328f690af7555a9f2e29c18dce02b717959edb0
Jakub Praszkowski <kubapraszkowski@gmail.com>
2024-03-04 18:38:15 +0100

    add: admin page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -11,6 +11,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
 import { authGuard } from './_guards/auth.guard';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { preventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { adminGuard } from './_guards/admin.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -23,7 +25,8 @@ const routes: Routes = [
       { path: 'members/:username', component: MemberDetailComponent },
       { path: 'member/edit', component: MemberEditComponent, canDeactivate: [preventUnsavedChangesGuard] },
       { path: 'lists', component: ListsComponent },
-      { path: 'messages', component: MessagesComponent }
+      { path: 'messages', component: MessagesComponent },
+      { path: 'admin', component: AdminPanelComponent, canActivate: [adminGuard] }
     ]
   },
   { path: 'errors', component: TestErrorComponent },
